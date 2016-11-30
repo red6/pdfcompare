75364bcc4e417b538423508e6fedacf1167c08b9
Malte Finsterwalder <malte.finsterwalder@red6-es.de>
2016-11-30 08:02:25 +0100

    Add a simple Viewer for side by side comparison

--- src/main/java/de/redsix/pdfcompare/DisplayMain.java
+++ src/main/java/de/redsix/pdfcompare/DisplayMain.java
@@ -6,9 +6,6 @@ public class DisplayMain {
 
     public static void main(String[] args) throws IOException {
         final CompareResult compareResult = new PdfComparator().compare("expected.pdf", "actual_marked.pdf");
-//        final CompareResult compareResult = new PdfComparator().compare("expected.pdf", "actual.pdf");
-        //        final CompareResult compareResult = new PdfComparator().compare("expected.pdf", "PdfMitBild.pdf");
-        //        final CompareResult compareResult = new PdfComparator().compare("PdfMitBild.pdf", "expected.pdf");
         new Display(compareResult);
     }
 }
