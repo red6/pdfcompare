8ae95686b114a18afe5cd799087be8d80522eaf8
Malte Finsterwalder <malte.finsterwalder@red6-es.de>
2016-12-07 09:58:03 +0100

    Move Main for testing to test directory; remove Test-Call from DisplayMain

--- src/main/java/de/redsix/pdfcompare/DisplayMain.java
+++ src/main/java/de/redsix/pdfcompare/DisplayMain.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 public class DisplayMain {
 
     public static void main(String[] args) throws IOException {
-        final CompareResult compareResult = new PdfComparator().compare("expected.pdf", "actual_marked.pdf");
         new Display(new CompareResult());
     }
 }
