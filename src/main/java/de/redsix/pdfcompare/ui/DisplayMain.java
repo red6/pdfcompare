c2c01597eb5080028e9cd8994a353eafb635f320
Malte Finsterwalder <malte.finsterwalder@red6-es.de>
2019-03-26 09:03:27 +0100

    readd DisplayMain

---
+++ src/main/java/de/redsix/pdfcompare/ui/DisplayMain.java
@@ -0,0 +1,10 @@
+package de.redsix.pdfcompare.ui;
+
+import java.io.IOException;
+
+public class DisplayMain {
+
+    public static void main(String[] args) throws IOException {
+        new Display().init();
+    }
+}
